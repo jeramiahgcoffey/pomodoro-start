977907e43f7a3635c5dd6d0bfbcb3945fbcf3bc8
[Jeramiah Coffey] <[jerrygcoffey@gmail.com]>
2021-08-11 15:17:20 -0500

    Added timer sessions, added reset functionality, fixed bug when pressing start multiple times.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,24 +10,56 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    start_button.config(command=start_timer)
+    window.after_cancel(timer)
+    reps = 0
+    timer_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    checkmark_label.config(text="")
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(10)
+    start_button.config(command=reset_timer)
+    global reps
+    reps += 1
+    if reps % 8 == 0:
+        count_down(2)
+        timer_label.config(text="Enjoy a break!", fg=GREEN)
+    elif reps % 2 == 0:
+        count_down(2)
+        timer_label.config(text="Enjoy a break!", fg=PINK)
+    else:
+        count_down(2)
+        timer_label.config(text="Time to Work!", fg=RED)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     minutes = math.floor(count / 60)
     seconds = count % 60
     # Dynamic typing
     if seconds < 10:
         seconds = f"0{seconds}"
-
     canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        checkmark_label.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -46,11 +79,10 @@ timer_label.grid(row=0, column=1)
 start_button = Button(text="Start", command=start_timer, highlightbackground=YELLOW)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", highlightbackground=YELLOW)
+reset_button = Button(text="Reset", command=reset_timer, highlightbackground=YELLOW)
 reset_button.grid(row=2, column=2)
 
-checkmark_label = Label(text="✔", font=(FONT_NAME, 32), fg=GREEN, bg=YELLOW)
+checkmark_label = Label(font=(FONT_NAME, 32), fg=GREEN, bg=YELLOW)
 checkmark_label.grid(row=3, column=1)
-
 
 window.mainloop()
